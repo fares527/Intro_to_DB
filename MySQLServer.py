3880838d8f385a058f80292982d877b16b85d4e8
fares ayman <149837282+fares527@users.noreply.github.com>
2025-02-07 21:03:24 +0200

    s

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,2 +1,28 @@
 import mysql.connector
 
+def create_database(host, user, password, database):
+
+   try:
+       mydb = mysql.connector.connect(
+           host = host,
+           user = user,
+           password = password,
+           
+        )
+       cursor = mydb.cursor()
+
+       cursor.execute(f"CREATE DATABASE IF NOT EXIST {database}")
+       mydb.commit()
+
+       print(f"Database '{database}' created successfully!")
+       return True
+   
+   except mysql.connector.Error as err:
+       print(f"Error creating database: {err}")
+       return False
+   
+   cursor.close()
+   mydb.close()
+       
+    
+
